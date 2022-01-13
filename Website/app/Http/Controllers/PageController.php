19df7a0c4cebf989d534307fd0dc5beb42588688
Slachy <slachy.dot@gmail.com>
2022-01-13 05:26:24 +0100

    v.0.2.5.4 Added direct McForum redirect

--- Website/app/Http/Controllers/PageController.php
+++ Website/app/Http/Controllers/PageController.php
@@ -17,8 +17,8 @@ class PageController extends Controller
         return view('Pages.Home.index');
     }
    /** Rederect to mcforum website */
-    public function apply(){
-        return view('Pages.Apply.index');
+    public function McForum(){
+        return redirect()->away('https://www.minecraftforum.net/forums/servers-java-edition/pc-servers/3022300-the-world-of-apollo-1-17-1-semi-vanilla-survival');
     }
    /** Apollo Discord join link  */
     public function Discord(){
--- Website/routes/web.php
+++ Website/routes/web.php
@@ -33,7 +33,8 @@ Route::get('/', function () {
 //* Used to link to different pages, making use of the PageController.php
 Route::get('/home', [PageController::class, 'Home'])->name('index');
 
-Route::get('/discord', [PageController::class, 'Discord'])->name('blog');
+Route::get('/discord', [PageController::class, 'Discord']);
+Route::get('/apply', [PageController::class, 'McForum']);
 Route::get('/test', [PageController::class, 'test'])->name('test');
 
 Route::get('/wiki', [PageController::class, 'wiki'])->name('index');
